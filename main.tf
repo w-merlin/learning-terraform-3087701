35a2170cff60d99477aa2cca3443c66b76516be0
w-merlin <59377428+w-merlin@users.noreply.github.com>
2022-11-15 21:54:04 -0800

    fixed error

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.blog.id]
-  
+
   tags = {
     Name = "HelloWorld"
   }
@@ -37,7 +37,7 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress'
+  type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
@@ -47,7 +47,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress'
+  type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 }
 
 resource "aws_security_group_rule" "blog_http_out" {
-  type        = "egress'
+  type        = "egress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
