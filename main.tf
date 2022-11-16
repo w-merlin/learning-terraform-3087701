2147ee2d8eef68d852694d483aeecbf834727bcb
w-merlin <59377428+w-merlin@users.noreply.github.com>
2022-11-16 10:05:21 -0800

    Using modules

--- main.tf
+++ main.tf
@@ -14,54 +14,42 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-    default = true
-}
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  name = "dev"
+  cidr = "10.0.0.0/16"
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
 
   tags = {
-    Name = "HelloWorld"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
-resource "aws_security_group" "blog" {
-    name = "blog"
-    description  = "Allow http and https in.  Allow everything out"
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-    vpc_id = data.aws_vpc.default.id
+  tags = {
+    Name = "Learning Terraform"
+  }
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["162.246.216.28/32"]
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["162.246.216.28/32"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_http_out" {
-  type        = "egress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["162.246.216.28/32"]
-
-  security_group_id = aws_security_group.blog.id
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
